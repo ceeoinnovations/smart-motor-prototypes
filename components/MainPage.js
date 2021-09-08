011e9a73912492d5149d8ba1767e02c741fe4fda
hyejinim <hyejinim17@gmail.com>
2021-09-07 23:24:29 -0400

    Update MainPage.js

--- components/MainPage.js
+++ components/MainPage.js
@@ -3,7 +3,7 @@ import About, {ShowHomeImage} from './About.js';
 import Footer from './Footer.js';
 import Projects, {ProjectItems, handleProjectFilter} from './Projects.js';
 
-
+// create mainpage (home)
 export default function MainPage(data){
     document.querySelector('.container').innerHTML = `
         ${Navbar(data.about)}
@@ -12,23 +12,4 @@ export default function MainPage(data){
         ${Footer(data.about)}
     `
     handleProjectFilter(data);
-    window.showThemes = function() {
-        console.log('main dropdown');
-        document.getElementById("myDropdown").classList.toggle("show");
-    }
-
-    // Close the dropdown if the user clicks outside of it
-    window.onclick = function(event) {
-        if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-            var i;
-            for (i = 0; i < dropdowns.length; i++) {
-                var openDropdown = dropdowns[i];
-                if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-                }
-            }
-        }
-    }
 }
-
